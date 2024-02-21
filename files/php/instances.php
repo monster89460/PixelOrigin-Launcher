1f0483be7768217295c27edbc9d05ca9ed8fbfff
monster89460 <160347151+monster89460@users.noreply.github.com>
2024-02-21 16:50:15 +0100

    Add files via upload

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,7 +1,7 @@
 <?php
-$instance['hypixel'] = array_merge($instance['hypixel'], array(
+$instance['PixelOrigin'] = array_merge($instance['PixelOrigin'], array(
     "loadder" => array(
-        "minecraft_version" => "1.8.9",
+        "minecraft_version" => "1.16.5",
         "loadder_type" => "forge",
         "loadder_version" => "latest"
     ),
@@ -21,26 +21,8 @@ $instance['hypixel'] = array_merge($instance['hypixel'], array(
     "whitelist" => array(),
     "whitelistActive" => false,
     "status" => array(
-        "nameServer" => "Hypixel",
-        "ip" => "mc.hypixel.net",
-        "port" => 25565
+        "nameServer" => "PixelOrigin",
+        "ip" => "pixelorigin.mine.fun",
+        "port" => 25571
     )
 ));
-
-$instance['PokeMoonX'] = array_merge($instance['PokeMoonX'], array(
-    "loadder" => array(
-        "minecraft_version" => "1.16.5",
-        "loadder_type" => "forge",
-        "loadder_version" => "1.16.5-36.2.35"
-    ),
-    "verify" => false,
-    "ignored" => array(),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "PokeMoonX",
-        "ip" => "178.32.106.234",
-        "port" => 25599
-    )
-));
-?>
